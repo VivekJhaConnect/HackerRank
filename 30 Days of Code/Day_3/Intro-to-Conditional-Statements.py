839f30e76fc61033b71e425e692b13607c42763d
VivekJhaConnect <vivekjhahsp@gmail.com>
2024-06-07 22:26:42 +0530

    Commiting 30 days of python coding

---
+++ 30 Days of Code/Day_3/Intro-to-Conditional-Statements.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    if N % 2 == 0:
+        if N>=6 and N<=20:
+            print("Weird")
+        else:
+            print("Not Weird")
+    else:
+        print("Weird")
+        
