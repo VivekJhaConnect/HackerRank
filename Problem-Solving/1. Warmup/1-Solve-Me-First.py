c824c0ddb111d6b6a31fce54bdd5fad8dcec3b05
VivekJhaConnect <vivekjhahsp@gmail.com>
2024-06-17 23:40:52 +0530

    Commit Problem Solving Code

---
+++ Problem-Solving/1. Warmup/1-Solve-Me-First.py
@@ -0,0 +1,19 @@
+'''
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Example:
+a = 7
+b = 3
+
+return 7+3=10
+'''
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a + b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
